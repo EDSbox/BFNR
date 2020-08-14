88f33a41d144faf69439f4d7b8e15f0c29709010
DDD_box <47934477+EDSbox@users.noreply.github.com>
2020-08-14 15:57:24 +0800

    Update BFNR.py

--- BFNR.py
+++ BFNR.py
@@ -1,7 +1,10 @@
-
+import keyboard
 import requests
 import time
+import xlsxwriter
 
+l = 0
+tn = str(time.strftime("%Y-%m-%d %H-%M-%S", time.localtime()))
 print("""
 
 ███████╗ ███████╗ ███╗   ██╗ ██████╗ 
@@ -14,119 +17,28 @@ print("""
 """) #这个是用 http://patorjk.com/software/taag/ 做的
 uid = int(input('输入你的B站UID:'))
 t = int(input('输入刷新时间间隔:'))
+print('按住E键终止记录(按的时间取决于你设置的时间间隔hhh)')
+name = str(uid)+' '+tn+".xlsx"
+workbook = xlsxwriter.Workbook(str(name))
+worksheet = workbook.add_worksheet(str(uid))
 url = 'https://api.bilibili.com/x/relation/stat?vmid=%s&jsonp=jsonp' % (uid)
-while (0 == 0):
-    a = ''
+while True:
     s = requests.get(url)
-    for __count in range(((129 - 7 * len(str((s.json()['data']['follower'])))) - 1)):
-        a = (a + ' ')
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "丨",s.json()["data"]["follower"])
     r = str((s.json()['data']['follower']))
-    L1 = a #从这里开始全都是数显的东西
-    L2 = a
-    L3 = a
-    L4 = a
-    L5 = a
-    for i in range(0, len(r)):
-        if (str(r[i]) == str('0')):
-            L1 = str((L1 + '██████ '))
-            L2 = str((L2 + '██  ██ '))
-            L3 = str((L3 + '██  ██ '))
-            L4 = str((L4 + '██  ██ '))
-            L5 = str((L5 + '██████ '))
-        if (str(r[i]) == str('1')):
-            L1 = str((L1 + '  ██   '))
-            L2 = str((L2 + '████   '))
-            L3 = str((L3 + '  ██   '))
-            L4 = str((L4 + '  ██   '))
-            L5 = str((L5 + '██████ '))
-        if (str(r[i]) == str('2')):
-            L1 = str((L1 + '██████ '))
-            L2 = str((L2 + '    ██ '))
-            L3 = str((L3 + '██████ '))
-            L4 = str((L4 + '██     '))
-            L5 = str((L5 + '██████ '))
-        if (str(r[i]) == str('3')):
-            L1 = str((L1 + '██████ '))
-            L2 = str((L2 + '    ██ '))
-            L3 = str((L3 + '██████ '))
-            L4 = str((L4 + '    ██ '))
-            L5 = str((L5 + '██████ '))
-        if (str(r[i]) == str('4')):
-            L1 = str((L1 + '██  ██ '))
-            L2 = str((L2 + '██  ██ '))
-            L3 = str((L3 + '██████ '))
-            L4 = str((L4 + '    ██ '))
-            L5 = str((L5 + '    ██ '))
-        if (str(r[i]) == str('5')):
-            L1 = str((L1 + '██████ '))
-            L2 = str((L2 + '██     '))
-            L3 = str((L3 + '██████ '))
-            L4 = str((L4 + '    ██ '))
-            L5 = str((L5 + '██████ '))
-        if (str(r[i]) == str('6')):
-            L1 = str((L1 + '██████ '))
-            L2 = str((L2 + '██     '))
-            L3 = str((L3 + '██████ '))
-            L4 = str((L4 + '██  ██ '))
-            L5 = str((L5 + '██████ '))
-        if (str(r[i]) == str('7')):
-            L1 = str((L1 + '██████ '))
-            L2 = str((L2 + '    ██ '))
-            L3 = str((L3 + '    ██ '))
-            L4 = str((L4 + '    ██ '))
-            L5 = str((L5 + '    ██ '))
-        if (str(r[i]) == str('8')):
-            L1 = str((L1 + '██████ '))
-            L2 = str((L2 + '██  ██ '))
-            L3 = str((L3 + '██████ '))
-            L4 = str((L4 + '██  ██ '))
-            L5 = str((L5 + '██████ '))
-        if (str(r[i]) == str('9')):
-            L1 = str((L1 + '██████ '))
-            L2 = str((L2 + '██  ██ '))
-            L3 = str((L3 + '██████ '))
-            L4 = str((L4 + '    ██ '))
-            L5 = str((L5 + '    ██ '))
-        if (str(r[i]) == str('-')):
-            L1 = str((L1 + '       '))
-            L2 = str((L2 + '       '))
-            L3 = str((L3 + '██████ '))
-            L4 = str((L4 + '       '))
-            L5 = str((L5 + '       '))
-        if (str(r[i]) == str(':')):
-            L1 = str((L1 + '       '))
-            L2 = str((L2 + '  ██   '))
-            L3 = str((L3 + '       '))
-            L4 = str((L4 + '  ██   '))
-            L5 = str((L5 + '       '))
-        if (str(r[i]) == str('丨')):
-            L1 = str((L1 + '  ██   '))
-            L2 = str((L2 + '  ██   '))
-            L3 = str((L3 + '  ██   '))
-            L4 = str((L4 + '  ██   '))
-            L5 = str((L5 + '  ██   '))
-        if (str(r[i]) == str(' ')):
-            L1 = str((L1 + '       '))
-            L2 = str((L2 + '       '))
-            L3 = str((L3 + '       '))
-            L4 = str((L4 + '       '))
-            L5 = str((L5 + '       '))
-    print('')
-    print('')
-    print('')
-    print('')
-    print('')
-    print('')
-    print(L1)
-    print(L2)
-    print(L3)
-    print(L4)
-    print(L5)
-    print('')
-    print('')
-    print('')
-    print('')
-    print('')
+    worksheet.write(l,0,tn)
+    worksheet.write(l,1,r)
+    l = l+1
+    try:
+        if keyboard.is_pressed('e'):
+            workbook.close()
+            print('终止记录')
+            break
+        else:
+            pass
+    except:
+        workbook.close()
+        print('程序遇到问题终止结果已保存')
+        break
     time.sleep(t)
 pass
