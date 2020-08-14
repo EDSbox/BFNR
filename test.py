ec3a522318b97ef6b362d8c3344af9c4cb827c9a
DDD_box <47934477+EDSbox@users.noreply.github.com>
2020-08-14 16:20:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-print("""
-██████╗ ███████╗███╗   ██╗██████╗ 
-██╔══ ██╗██╔════╝████╗  ██║██╔══██╗
-██████╔╝█████╗  ██╔██╗ ██║██████╔╝
-██╔══██╗██╔══╝  ██║╚██╗██║██╔══██╗
-██████╔╝██║     ██║ ╚████║██║  ██║
-╚═════╝ ╚═╝     ╚═╝  ╚═══╝╚═╝  ╚═╝
-                                  
-""")
+import xlsxwriter
+import time
+import keyboard
+
+test = 0
+uid = 2333
+time = str(time.strftime("%Y-%m-%d %H-%M-%S", time.localtime()))
+name = str(uid)+' '+time+".xlsx"
+workbook = xlsxwriter.Workbook(str(name))
+worksheet = workbook.add_worksheet(str(uid))
+worksheet.write(0,0,'0.0')
+worksheet.write(1,0,'1.0')
+worksheet.write(0,1,'0.1')
+workbook.close()
