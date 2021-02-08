9b18f18c4f9320d9824b54f36d07990e14c00d1e
EDS box <a2356960576@163.com>
2021-02-08 23:59:59 +0800

    新增零点时保存功能

--- BFNR.py
+++ BFNR.py
@@ -17,16 +17,18 @@ print("""
 """) #这个是用 http://patorjk.com/software/taag/ 做的
 uid = int(input('输入你的B站UID:'))
 t = int(input('输入刷新时间间隔:'))
-print('按住E键终止记录(按的时间取决于你设置的时间间隔hhh)')
+print('按住E键终止记录(按下时间取决于你设置的时间间隔)')
 name = str(uid)+' '+tn+".xlsx"
+save = 1
 workbook = xlsxwriter.Workbook(str(name))
 worksheet = workbook.add_worksheet(str(uid))
 url = 'https://api.bilibili.com/x/relation/stat?vmid=%s&jsonp=jsonp' % (uid)
 while True:
     s = requests.get(url)
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "丨",s.json()["data"]["follower"])
+    ti = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print(ti, "丨",s.json()["data"]["follower"])
     r = str((s.json()['data']['follower']))
-    worksheet.write(l,0,tn)
+    worksheet.write(l,0,ti)
     worksheet.write(l,1,r)
     l = l+1
     try:
@@ -34,11 +36,23 @@ while True:
             workbook.close()
             print('终止记录')
             break
+        if str(time.strftime("%H:%M", time.localtime())) == '00:00':
+            if save == 0:
+                save = 1
+                workbool.close()
+                name = str(uid)+' '+tn+".xlsx"
+                workbook = xlsxwriter.Workbook(str(name))
+                worksheet = workbook.add_worksheet(str(uid))
+                pass
+            pass
+        if str(time.strftime("%H:%M", time.localtime())) == '00:01':
+            save = 0 
+            pass
         else:
             pass
     except:
         workbook.close()
-        print('程序遇到问题终止结果已保存')
+        print('程序遇到问题终止,结果已保存')
         break
     time.sleep(t)
 pass
